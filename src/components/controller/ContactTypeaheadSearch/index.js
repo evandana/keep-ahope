d1840be693adbda7d4cc1e23637fe91bcea518ff
Baluha, Alex <alexbaluha@gmail.com>
2018-05-13 15:42:27 -0400

    search button and char count text

--- src/components/controller/ContactTypeaheadSearch/index.js
+++ src/components/controller/ContactTypeaheadSearch/index.js
@@ -1,18 +1,23 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { searchContacts, setCurrentSearchQuery } from 'actions';
 import ContactTypeaheadSearch from 'components/view/ContactTypeaheadSearch';
 
 const mapStateToProps = (state) => {
     return {
         dataSource: state.contacts.searchResults,
+        contactSearchQuery: state.contacts.searchQuery,
     };
 };
 
 const mapDispatchToProps = {
     typeaheadSearch: searchContacts,
+    setCurrentSearchQuery,
 };
+
+const ContactTypeaheadSearchWithRouter = withRouter(ContactTypeaheadSearch);
 
 export default connect(
     mapStateToProps,
     mapDispatchToProps,
-)(ContactTypeaheadSearch);
+)(ContactTypeaheadSearchWithRouter);
--- src/components/view/ContactTypeaheadSearch/index.js
+++ src/components/view/ContactTypeaheadSearch/index.js
@@ -1,33 +1,75 @@
 import React, {Component} from 'react';
+
 import AutoComplete from 'material-ui/AutoComplete';
+import CancelIcon from 'material-ui/svg-icons/navigation/cancel';
 import Paper from 'material-ui/Paper';
 
-import CancelIcon from 'material-ui/svg-icons/navigation/cancel';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 
 class ContactTypeaheadSearch extends Component {
 
     handleUpdateInput = (value) => {
-        if (value.length > 0) {
+        if (value.length > 0 && value.length < 16 /* TODO: validate entry regex here */) {
             this.props.typeaheadSearch(value);
+
+            // navigate to contact search results page if we're not already there
+            if (this.props.location.pathname !== '/contact') {
+                this.props.history.push('/contact');
+            }
         }
+
     };
 
     render() {
-        const { dataSource } = this.props;
+        const {
+            dataSource,
+            contactSearchQuery,
+            setCurrentSearchQuery,
+            muiTheme: {palette},
+        } = this.props;
+
         return (
-            <Paper style={{display: 'flex', alignItems: 'baseline', width: 'auto', padding: '0.5rem'}} zDepth={1} >
+            <Paper
+                style={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    width: 'auto',
+                    padding: '0.5rem',
+                    position: 'relative', // cancel and char count are absolutely positioned
+                }}
+                zDepth={1}
+            >
                 <AutoComplete
-                    menuStyle={{display: 'none'}} // FIXME:
                     dataSource={dataSource}
                     floatingLabelText="Search for a User ID"
                     fullWidth={true}
                     hintText="AA BB 01 01 2018 AAA"
-                    onNewRequest={(c) => {console.log(`go to contact/${c}/`)}}
+                    menuStyle={{display: 'none'}} // FIXME:
                     onUpdateInput={this.handleUpdateInput}
+                    searchText={contactSearchQuery}
                 />
+                <CancelIcon
+                    color={palette.accent3Color}
+                    style={{
+                        position: 'absolute',
+                        top: '2.5rem',
+                        right: '0.4rem',
+                        cursor: 'pointer',
+                        zIndex: '1101',
+                    }}
+                    onClick={() => {setCurrentSearchQuery('')}}
+                />
+                <div style={{
+                    position: 'absolute',
+                    right: '0.5rem',
+                    bottom: '-1rem',
+                    fontSize: '0.6rem',
+                }}>
+                    {contactSearchQuery.length} / 15
+                </div>
             </Paper>
         );
     }
 }
 
-export default ContactTypeaheadSearch;
+export default muiThemeable()(ContactTypeaheadSearch);
